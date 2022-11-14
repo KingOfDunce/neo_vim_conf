61b4d99cf0c8229baf1f7380faaecf0a5984b7a2
Eric Kirchner <kirchner.eric.ek@gmail.com>
2022-11-14 17:46:04 +0100

    Push config files

---
+++ lua/keys.lua
@@ -0,0 +1,18 @@
+--[[ keys.lua ]]
+local map = vim.api.nvim_set_keymap
+
+-- remap the key used to leave insert mode
+map('i', 'jk', '<esc>', {})
+
+-- Toggle nvim-tree
+map('n', 'tn', [[:NvimTreeToggle]], {})
+
+-- Toggle more plugins
+map('n', 'tt', [[:TagbarToggle]], {})
+map('n', '<F2>', [[:Telescope find_files]], {})
+map('n', 'tl', [[:IndentLinesToggle]], {})
+
+-- Terminal
+map('n', '<F5>', [[:tabnew<CR>:terminal<CR>A]], {})
+map('n', '<F6>', [[:let $VIM_DIR=expand('%:p:h')<CR>:tabnew<CR>:terminal<CR>Acd $VIM_DIR<CR>]], {})
+map('t', '<Esc>', [[<C-\><C-n>]], {})
---
+++ lua/plug.lua
@@ -0,0 +1,51 @@
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- [[ Dev ]]
+  use { 'johnfrankmorgan/whitespace.nvim' }
+  use {
+    'nvim-telescope/telescope.nvim',                 -- fuzzy finder
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use { 'majutsushi/tagbar' }                        -- code structure
+  use { 'Yggdroot/indentLine' }                      -- see indentation
+  use { 'tpope/vim-fugitive' }                       -- git integration
+  use { 'junegunn/gv.vim' }                          -- commit history
+  use { 'windwp/nvim-autopairs' }
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+  use { 'williamboman/mason.nvim' }
+  use { 'williamboman/mason-lspconfig.nvim' }
+  use { 'neovim/nvim-lspconfig' }
+  use 'puremourning/vimspector'
+  -- [[ Rust ]]
+  use { 'simrat39/rust-tools.nvim' }
+  use 'hrsh7th/nvim-cmp'
+  -- LSP completion source:
+  use 'hrsh7th/cmp-nvim-lsp'
+
+  -- Useful completion sources:
+  use 'hrsh7th/cmp-nvim-lua'
+  use 'hrsh7th/cmp-nvim-lsp-signature-help'
+  use 'hrsh7th/cmp-vsnip'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/vim-vsnip'
+  -- [[ Theme ]]
+  use { 'mhinz/vim-startify' }                       -- start screen
+  use { 'DanilaMihailov/beacon.nvim' }               -- cursor jump
+  use {                                              -- filesystem navigation
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons'        -- filesystem icons
+  }
+  use {
+    'nvim-lualine/lualine.nvim',                     -- statusline
+    requires = {'kyazdani42/nvim-web-devicons',
+                opt = true}
+  }
+  use 'folke/lsp-colors.nvim'
+  use 'folke/tokyonight.nvim'
+end)
